6a159c3ee09d35ed14926636cfe616cd00affd72
Todd Heidenreich <jtheidenreich@gmail.com>
2014-03-10 08:32:23 -0400

    added google maps

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -36,3 +36,42 @@ $('.upload-button').click(function(){
 	$('.js-time').val('')
 
 })
+
+// google.maps.event.addListener(map, "click", function (event) {
+//     var latitude = event.latLng.lat();
+//     var longitude = event.latLng.lng();
+//     console.log( latitude + ', ' + longitude );
+// });
+
+
+function initialize() {
+    var mapOptions = {
+        zoom: 4,
+        center: new google.maps.LatLng(41.5008195, -99.680902),
+        mapTypeId: google.maps.MapTypeId.TERRAIN
+    };
+
+    var map = new google.maps.Map(document.getElementById('map-canvas'),
+        mapOptions);
+
+   
+    // var flightPath = new google.maps.Polyline({
+    //     path: flightPlanCoordinates,
+    //     geodesic: true,
+    //     strokeColor: '#FF0000',
+    //     strokeOpacity: 1.0,
+    //     strokeWeight: 2
+    // });
+
+    // flightPath.setMap(map);
+
+
+    google.maps.event.addListener(map, "click", function(event) {
+        var latitude = event.latLng.lat();
+        var longitude = event.latLng.lng();
+        console.log(latitude + ', ' + longitude);
+    });
+
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
